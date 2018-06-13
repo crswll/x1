719c1de0dd704c668ab3f3a7aef6cfdcdf2d9b9c
Bill Criswell <Bill_Criswell@comcast.com>
2018-06-13 19:49:09 -0400

    adjusments

--- src/http.js
+++ src/http.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 const timeout = 1000
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://crswll.github.io/x1/' : ''
 
 const http = axios.create({
   timeout,
+  baseURL,
 })
 
 export default http
